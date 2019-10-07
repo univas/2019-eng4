4676a3c77d737a504bfb70e7d5a0c80a576b2a81
Rodrigo Luis de Faria <rodrigo.faria@inatel.br>
2019-10-07 19:51:36 -0300

    Update builder project

--- builder/src/br/edu/univas/App.java
+++ builder/src/br/edu/univas/App.java
@@ -3,9 +3,23 @@ package br.edu.univas;
 public class App {
 
 	public static void main(String[] args) {
-		BankAccount account1 = new BankAccount(190, "João", "0928-1", 100, 1.99);
+		//BankAccount account1 = new BankAccount(190, "João", "0928-1", 100, 1.99);
+		BankAccount account1 = new BankAccount.Builder()
+				.withBalance(100)
+				.withInterestRate(1.99)
+				.withBranch("0928-1")
+				.withAccountNumber(190)
+				.withOwner("João")
+				.build();
 		
-		BankAccount account2 = new BankAccount(199, "Maria", null, 2983, 0.88);
+		
+		//BankAccount account2 = new BankAccount(199, "Maria", null, 2983, 0.88);
+		BankAccount account2 = new BankAccount.Builder()
+				.withAccountNumber(199)
+				.withOwner("Maria")
+				.withBalance(2983)
+				.withInterestRate(0.88)
+				.build();
 		
 		System.out.println("Account 1:");
 		System.out.println(account1);
--- builder/src/br/edu/univas/BankAccount.java
+++ builder/src/br/edu/univas/BankAccount.java
@@ -8,13 +8,52 @@ public class BankAccount {
     private double balance;
     private double interestRate;
     
-	public BankAccount(long accountNumber, String owner, String branch, double balance, double interestRate) {
+	private BankAccount(long accountNumber, String owner, String branch, double balance, double interestRate) {
 		this.accountNumber = accountNumber;
 		this.owner = owner;
 		this.branch = branch;
 		this.balance = balance;
 		this.interestRate = interestRate;
 	}
+	
+	public static class Builder {
+		private long accountNumber;
+	    private String owner;
+	    private String branch;
+	    private double balance;
+	    private double interestRate;
+	    
+	    public Builder withAccountNumber(long accountNumber) {
+	    	this.accountNumber = accountNumber;
+	    	return this;
+	    }
+	    
+	    public Builder withOwner(String owner) {
+	    	this.owner = owner;
+	    	return this;
+	    }
+	    
+	    public Builder withBranch(String branch) {
+	    	this.branch = branch;
+	    	return this;
+	    }
+	    
+	    public Builder withBalance(double balance) {
+	    	this.balance = balance;
+	    	return this;
+	    }
+	    
+	    public Builder withInterestRate(double interestRate) {
+	    	this.interestRate = interestRate;
+	    	return this;
+	    }
+	    
+	    public BankAccount build() {
+	    	return new BankAccount(accountNumber, owner, 
+	    			branch, balance, interestRate);
+	    }
+	}
+	
 
 	public long getAccountNumber() {
 		return accountNumber;
