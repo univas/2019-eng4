da192c06da0027d561973f069ea97d463928258c
Rodrigo Luis de Faria <rodrigo.faria@inatel.br>
2019-10-28 18:59:05 -0300

    Add bridge

---
+++ bridge/src/br/edu/univas/App.java
@@ -0,0 +1,16 @@
+package br.edu.univas;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		Shape redCircle = new RedCircle();
+		
+		Shape blueCircle = new BlueCircle();
+		
+		Shape redSquare = new RedSquare();
+		
+		Shape blueSquare = new BlueSquare();
+		
+	}
+}
---
+++ bridge/src/br/edu/univas/BlueCircle.java
@@ -0,0 +1,5 @@
+package br.edu.univas;
+
+public class BlueCircle extends Shape {
+
+}
